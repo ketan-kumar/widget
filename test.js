9f4cbe8f3ead26767a7aa77447ec7dd0a7f3bff8
KG Suryavanshi <kgsuryavanshi@KGs-MacBook-Pro.local>
2023-01-09 18:32:09 +0530

    fix: apply target

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 window.onload = (event) => {
   console.log('dom loaded =============', event);
+  const window = event.target;
   const ele = window.getElementById('gmi-widget-form');
   console.log('element -------------', ele);
 };
