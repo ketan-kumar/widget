358c529f21e65619ae410463ad6f76b4085b189b
KG Suryavanshi <kgsuryavanshi@KGs-MacBook-Pro.local>
2023-01-09 18:24:29 +0530

    fix: chnaged function call

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
-window.onload(() => {
+function main() {
   console.log('dom loaded =============');
   const ele = window.getElementById('gmi-widget-form');
   console.log('element -------------', ele);
-});
+};
+
+window.onload = main;
