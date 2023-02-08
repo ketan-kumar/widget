1b1f49911c8e81b7486be025e98ad02f632c18d8
Prachi-2002 <prachimadame01@gmail.com>
2023-02-08 14:14:53 +0530

    added the code for toggle options

--- test.js
+++ test.js
@@ -338,14 +338,25 @@ window.onload = (event) => {
   console.log(idOfWidget,'Id of widget ======')
   console.log(data,'Data ========')
   const dataOfWidget = widgetCall(idOfWidget);
-  // const widgetDetails = dataOfWidget.data[0];
-  // console.log(widgetDetails,'widget details =======');
+  const statusObject = {
+    active:1,
+    inactive:0
+  }
+  const widgetDetails = dataOfWidget.data[0];
+  const widgetMeta = JSON.parse(widgetDetails.widget_meta);
+
+  console.log(widgetDetails,'widget details =======');
+  console.log(widgetMeta,'Widget Meta ========')
   console.log(dataOfWidget,'data widget ========');
-  const statusOfWidget = true;
-  const nameOption = true;
-  const emailOption = false;
-  const surnameOption = false;
-  const couponOption = true;
+  const statusOfWidget = statusObject[widgetDetails.status];
+  console.log(statusOfWidget ,'status of widget =============')
+  const nameOption = statusObject[widgetMeta.should_name_mount];
+  console.log(nameOption,'name Option =======')
+  // const emailOption = false;
+  const surnameOption = statusObject[widgetMeta.should_surname_mount];
+  console.log(surnameOption, "surnameOtion =======")
+  const couponOption = statusObject[widgetMeta.should_coupon_mount];
+  console.log(couponOption,'Coupon option =======')
   const ele = window.getElementById("gmi-registration-widget");
   ele.innerHTML = statusOfWidget ? `<div id="gmi-widget-form">
   <div id="first-modal" style="display:block;">
@@ -366,10 +377,10 @@ window.onload = (event) => {
   <svg xmlns="http://www.w3.org/2000/svg" width="40px" height="35px" viewBox="0 0 24 24"><g data-name="Layer 2"><path fill="rgba(0, 0, 0, 0.2)" d="M12 11a4 4 0 1 0-4-4 4 4 0 0 0 4 4zm6 10a1 1 0 0 0 1-1 7 7 0 0 0-14 0 1 1 0 0 0 1 1z" data-name="person"/></g></svg>                  
   <input type="text" placeholder="Cognome" id="surname" value='${surname}' onKeyup = "myFunc()" />
   </div>`: ""}
-  ${emailOption ? `<div class="element-container">
+ <div class="element-container">
   <?xml version="1.0" ?><!DOCTYPE svg  PUBLIC '-//W3C//DTD SVG 1.1//EN'  'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'><svg width="40px" height="35px" id="Layer_1" style="enable-background:new 0 0 512 512;" version="1.1" viewBox="0 0 512 512"  xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><path fill="rgba(0, 0, 0, 0.2)" d="M67,148.7c11,5.8,163.8,89.1,169.5,92.1c5.7,3,11.5,4.4,20.5,4.4c9,0,14.8-1.4,20.5-4.4c5.7-3,158.5-86.3,169.5-92.1   c4.1-2.1,11-5.9,12.5-10.2c2.6-7.6-0.2-10.5-11.3-10.5H257H65.8c-11.1,0-13.9,3-11.3,10.5C56,142.9,62.9,146.6,67,148.7z"/><path fill="rgba(0, 0, 0, 0.2)" d="M455.7,153.2c-8.2,4.2-81.8,56.6-130.5,88.1l82.2,92.5c2,2,2.9,4.4,1.8,5.6c-1.2,1.1-3.8,0.5-5.9-1.4l-98.6-83.2   c-14.9,9.6-25.4,16.2-27.2,17.2c-7.7,3.9-13.1,4.4-20.5,4.4c-7.4,0-12.8-0.5-20.5-4.4c-1.9-1-12.3-7.6-27.2-17.2l-98.6,83.2   c-2,2-4.7,2.6-5.9,1.4c-1.2-1.1-0.3-3.6,1.7-5.6l82.1-92.5c-48.7-31.5-123.1-83.9-131.3-88.1c-8.8-4.5-9.3,0.8-9.3,4.9   c0,4.1,0,205,0,205c0,9.3,13.7,20.9,23.5,20.9H257h185.5c9.8,0,21.5-11.7,21.5-20.9c0,0,0-201,0-205   C464,153.9,464.6,148.7,455.7,153.2z"/></g></svg>                 
   <input type="text" placeholder="Email" id="email" value='${email}' onclick="emailClickFunc()" onKeyup="emailFunc()" />
-  </div>`: ""}
+  </div>
   <div id='hiding-email-msg' style="display:none;margin-bottom: 1em;
   color: red;">* please enter your email</div>
   </form>
