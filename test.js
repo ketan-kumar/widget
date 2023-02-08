3f7717eed5533e20dd6acaf99376dae1b80ed14c
Prachi-2002 <prachimadame01@gmail.com>
2023-02-08 17:26:39 +0530

    added the code for api call

--- test.js
+++ test.js
@@ -121,7 +121,7 @@ function couponFunction() {
     document.getElementById("coupon-tab").style.marginBottom = "0px";
   }
 }
-async function handleButton() {
+async function handleButton(nameOption, surnameOption, couponOption) {
   let campaign = "";
   const params = window.location.search.substr(1).split("&");
   for (const param of params) {
@@ -134,10 +134,10 @@ async function handleButton() {
       campaign = splitParam[1];
     }
   }
-  name = document.getElementById("name").value || '';
-  email = document.getElementById("email").value || '';
-  surname = document.getElementById("surname").value || '';
-  couponCode = document.getElementById("coupon").value || '';
+  name = nameOption === 1 ? document.getElementById("name").value : '';
+  email = document.getElementById("email").value;
+  surname = surnameOption === 1 ? document.getElementById("surname").value : '';
+  couponCode = couponOption === 1 ? document.getElementById("coupon").value : '';
   let apiResp;
   const userData = {
     action: "signup",
@@ -342,7 +342,7 @@ window.onload = async(event) => {
   </div>`: ""}
  <div class="element-container">
   <?xml version="1.0" ?><!DOCTYPE svg  PUBLIC '-//W3C//DTD SVG 1.1//EN'  'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'><svg width="40px" height="35px" id="Layer_1" style="enable-background:new 0 0 512 512;" version="1.1" viewBox="0 0 512 512"  xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><path fill="rgba(0, 0, 0, 0.2)" d="M67,148.7c11,5.8,163.8,89.1,169.5,92.1c5.7,3,11.5,4.4,20.5,4.4c9,0,14.8-1.4,20.5-4.4c5.7-3,158.5-86.3,169.5-92.1   c4.1-2.1,11-5.9,12.5-10.2c2.6-7.6-0.2-10.5-11.3-10.5H257H65.8c-11.1,0-13.9,3-11.3,10.5C56,142.9,62.9,146.6,67,148.7z"/><path fill="rgba(0, 0, 0, 0.2)" d="M455.7,153.2c-8.2,4.2-81.8,56.6-130.5,88.1l82.2,92.5c2,2,2.9,4.4,1.8,5.6c-1.2,1.1-3.8,0.5-5.9-1.4l-98.6-83.2   c-14.9,9.6-25.4,16.2-27.2,17.2c-7.7,3.9-13.1,4.4-20.5,4.4c-7.4,0-12.8-0.5-20.5-4.4c-1.9-1-12.3-7.6-27.2-17.2l-98.6,83.2   c-2,2-4.7,2.6-5.9,1.4c-1.2-1.1-0.3-3.6,1.7-5.6l82.1-92.5c-48.7-31.5-123.1-83.9-131.3-88.1c-8.8-4.5-9.3,0.8-9.3,4.9   c0,4.1,0,205,0,205c0,9.3,13.7,20.9,23.5,20.9H257h185.5c9.8,0,21.5-11.7,21.5-20.9c0,0,0-201,0-205   C464,153.9,464.6,148.7,455.7,153.2z"/></g></svg>                 
-  <input type="text" placeholder="Email" id="email" value='${email}' onclick="emailClickFunc()" onKeyup="emailFunc()" />
+  <input type="text" placeholder="Email" id="email" value='${email}' onclick="emailClickFunc()" onKeyup="emailFunc(${nameOption}, ${surnameOption}, ${couponOption})" />
   </div>
   <div id='hiding-email-msg' style="display:none;margin-bottom: 1em;
   color: red;">* please enter your email</div>
@@ -371,7 +371,7 @@ window.onload = async(event) => {
         <div class="coupon-tab2" id='coupon-button' onClick="couponDetails()"><button id='add-button'>ADD</button></div></div><div id='coupon-resp' style="color:black; display:block"></div>
   </div>
   <div id="coupon-code">
-  <div class="element-container" onclick="handleButton()"><button id='free-trial'>INIZIA ORA</button></div>
+  <div class="element-container" onclick="handleButton(${nameOption}, ${surnameOption}, ${couponOption})"><button id='free-trial'>INIZIA ORA</button></div>
   <div id="api-error-msg" style="color: red; margin-bottom: 1em;"></div>
   <div>Sei già registrato? <a href='https://app.dev.goodmorningitalia.it/login'><b>Fai il login qui</b></a></div>
   </div>
